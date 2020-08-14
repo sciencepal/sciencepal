65228c95fbd326cc0f038b3d7bc213b3ee896d32
Aditya Pal <aditya.pal.science@gmail.com>
2020-08-14 13:30:27 +0530

    Update generate_random_workflow.py

--- generate_random_workflow.py
+++ generate_random_workflow.py
@@ -1,6 +1,10 @@
 import os
 import random
 
+class SafeDict(dict):
+  def __missing__(self, key):
+    return '{' + key + '}'
+
 with open("workflow.txt", "r") as f:
   wf = f.read()
-print (wf.format(random_number=random.randint(1,8)))
+print (wf.format_map(SafeDict(random_number=random.randint(1,8))))
