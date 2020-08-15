0dcc4a254925e1fe6a3b4c79bc52a0ce97dadb04
Aditya Pal <aditya.pal.science@gmail.com>
2020-08-15 14:46:12 +0530

    Changed token in checkout step :)

--- generate_random_workflow.py
+++ generate_random_workflow.py
@@ -1,12 +1,6 @@
 import os
 import random
 
-class SafeDict(dict):
-  def __missing__(self, key):
-    if '.' in key:
-      return '{{' + key + '}}'
-    return '{' + key + '}'
-
 with open("workflow.txt", "r") as f:
   wf = f.read()
-print (wf.format_map(SafeDict(random_number=random.randint(1,8))))
+print (wf.replace("{random_number}", str(random.randint(1, 8))))
