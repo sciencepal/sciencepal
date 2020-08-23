410e2de739bec29a84544bb98ef02e81d2ed1248
Aditya Pal <aditya.pal.science@gmail.com>
2020-08-23 17:47:14 +0530

    Added Assets and separated codes in folders

--- codes/generate_random_workflow.py
+++ codes/generate_random_workflow.py
@@ -1,6 +1,6 @@
 import os
 import random
 
-with open("workflow.txt", "r") as f:
+with open("../assets/workflow.txt", "r") as f:
   wf = f.read()
 print (wf.replace("{random_number}", str(random.randint(1, 8))))
