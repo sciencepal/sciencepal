683423d62830266d26d65f414b1295cc5113b0c3
Aditya Pal <aditya.pal.science@gmail.com>
2020-09-05 02:18:06 +0530

    Update generate_random_workflow.py

--- codes/generate_random_workflow.py
+++ codes/generate_random_workflow.py
@@ -1,6 +1,17 @@
 import os
 import random
 
-with open("assets/workflow.txt", "r") as f:
+cron_line = '    - cron: "0 */{prevNo} * * *"'
+
+with open(".github/workflows/rating-chart.yml", "r") as f:
   wf = f.read()
-print (wf.replace("{random_number}", str(random.randint(1, 8))))
+  
+randNo = random.randint(1, 8)
+newCron = cron_line.format(prevNo=randNo)
+
+for prevNum in range (1, 9):
+  prevCron = cron_line.format(prevNo=prevNum)
+  if wf.find(prevCon) != -1:
+    wf.replace(prevCron, newCron)
+    break
+print (wf)
