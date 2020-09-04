0e7c1e3f3b35f93070b5af699c52eed96adf151c
Aditya Pal <aditya.pal.science@gmail.com>
2020-09-05 02:30:30 +0530

    Bug Fix

--- codes/generate_random_workflow.py
+++ codes/generate_random_workflow.py
@@ -1,7 +1,7 @@
 import os
 import random
 
-cron_line = '    - cron: "0 */{prevNo} * * *"'
+cron_line = '"0 */{prevNo} * * *"'
 
 with open(".github/workflows/rating-chart.yml", "r") as f:
   wf = f.read()
@@ -12,6 +12,6 @@ newCron = cron_line.format(prevNo=randNo)
 for prevNum in range (1, 9):
   prevCron = cron_line.format(prevNo=prevNum)
   if wf.find(prevCron) != -1:
-    wf.replace(prevCron, newCron)
+    wf = wf.replace(prevCron, newCron)
     break
 print (wf)
